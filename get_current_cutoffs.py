3c714279e491a5923a76899739bcf93de2e63e96
Jacob Lindelof <jlindelof@me.com>
2020-11-22 03:01:41 -0500

    new cutoffs

--- get_current_cutoffs.py
+++ get_current_cutoffs.py
@@ -269,9 +269,6 @@ def get_current_cutoffs():
 
     generate_chart_data(cutoffs)
 
-    page_file = open('last_page.txt', 'w')
-    page_file.write(last_page)
-    page_file.close()
 
 if __name__ == "__main__":
     get_current_cutoffs()
